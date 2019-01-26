767639bcc447b5a4a430c7c3ee1a8326d59783e8
nimil <nimilgg@qq.com>
2019-01-26 14:40:16 +0800

    桥接模式

---
+++ src/main/java/xin/nimil/design/strutual/brige/SavingAccount.java
@@ -0,0 +1,9 @@
+package xin.nimil.design.strutual.brige;
+
+/**
+ * @Author:nimil e-mail:nimilgg@qq.com
+ * @Date:2019/1/26
+ * @Time:10:37
+ */
+public class SavingAccount {
+}
---
+++ src/main/java/xin/nimil/design/strutual/brige/Test.java
@@ -0,0 +1,9 @@
+package xin.nimil.design.strutual.brige;
+
+/**
+ * @Author:nimil e-mail:nimilgg@qq.com
+ * @Date:2019/1/26
+ * @Time:10:49
+ */
+public class Test {
+}
